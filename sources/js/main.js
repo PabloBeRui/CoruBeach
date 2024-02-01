2d9cb78d1d5de686d9ab17128a9112c760cf9fdf
PabloBeRui <121795105+PabloBeRui@users.noreply.github.com>
2024-02-01 12:17:39 +0100

    Search bar implementation

--- sources/js/main.js
+++ sources/js/main.js
@@ -1,45 +1,74 @@
-const beachArray = [
-  { beachName: "Riazor", photo: 1 },
-  { beachName: "Riazor2", photo: 2 },
-  { beachName: "Riazor3", photo: 3 },
-  { beachName: "Riazor4", photo: 4 },
-];
-let mainButton = document.getElementById("mainButton");
+export const main = () => {
+  const beachArray = [
+    { beachName: "Riazor", photo: 1 },
+    { beachName: "Orzan", photo: 2 },
+    { beachName: "Lapas", photo: 3 },
+    { beachName: "Caion", photo: 4 },
+  ];
+  let mainButton = document.getElementById("mainButton");
 
-let main = document.getElementById("main");
-let cardsContainerDiv = document.createElement("div");
-cardsContainerDiv.setAttribute("id", "cardsContainerDiv");
+  let main = document.getElementById("main");
+  let cardsContainerDiv = document.createElement("div");
+  cardsContainerDiv.setAttribute("id", "cardsContainerDiv");
 
-mainButton.addEventListener("click", () => {
-  mainButton.style.display = "none";
-  main.appendChild(cardsContainerDiv);
+  //!Creamos los componentes del buscador
+  let searchDiv = document.createElement("div");
+  let searchInput = document.createElement("input");
+  let searchButton = document.createElement("button");
 
-    for (let beach of beachArray) {
+  searchButton.innerText = "Buscar";
+  searchButton.id = "searchButton";
+
+  searchInput.placeholder = "busca tu playa";
+  searchInput.id = "searchInput";
+
+  //! Cuando se aprieta el boton , hacemos que desaparezca el boton y cargue un nuevo div, que será el contenedor de las cartas
+
+  mainButton.addEventListener("click", () => {
+    mainButton.style.display = "none";
+    searchDiv.appendChild(searchInput);
+    searchDiv.appendChild(searchButton);
+    main.appendChild(searchDiv);
+
+    main.appendChild(cardsContainerDiv);
+
+    renderMain(beachArray);
+  });
+
+  searchButton.addEventListener("click", () => {
+    if (searchInput.value === "") {
+      return;
+    } else {
+      //!Filtramos el array con el valor que nos pasa el usuario en el input y hacemos un nuevo renderizado con el array filtrado
+      let pattern = searchInput.value.toLowerCase();
+      let filtered = beachArray.filter((beach) =>
+      beach.beachName.toLowerCase().includes(pattern));
       
+      if(filtered.length === 0){
+        alert("No se ha encontrado ningún resultado");
+        return;
+      }
+      cardsContainerDiv.innerHTML = '';
+      renderMain(filtered);
+    }
+  });
 
-    let cardDiv = document.createElement("div");
-    cardDiv.classList.add("cardDiv");
+  function renderMain(userArray) {
+    for (let beach of userArray) {
+      let cardDiv = document.createElement("div");
+      cardDiv.classList.add("cardDiv");
 
-    let imgCard = document.createElement("img");
-    imgCard.classList.add("imgCard");
-    imgCard.setAttribute("src", `./sources/img/beaches/${beach.photo}.jpeg`);
+      let imgCard = document.createElement("img");
+      imgCard.classList.add("imgCard");
+      imgCard.setAttribute("src", `./sources/img/beaches/${beach.photo}.jpeg`);
 
-    let pCard = document.createElement("p");
-    pCard.classList.add("pCard");
-        pCard.innerText = `${beach.beachName}`;
-        
-        
+      let pCard = document.createElement("p");
+      pCard.classList.add("pCard");
+      pCard.innerText = `${beach.beachName}`;
 
-    console.log(imgCard);
-      console.log(pCard);
-      
-      cardDiv.appendChild(imgCard)  
-        cardDiv.appendChild(pCard)  
-        cardsContainerDiv.appendChild(cardDiv)
-      
-
-    //   console.log(beach)
-
-    //   cardDiv.appendChild()
+      cardDiv.appendChild(imgCard);
+      cardDiv.appendChild(pCard);
+      cardsContainerDiv.appendChild(cardDiv);
+    }
   }
-});
+};
